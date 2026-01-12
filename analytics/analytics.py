4a9938f6138bcaeae3d88e0ba5e342be57c574c5
Hung Hoang Quoc <hqhung0306@gmail.com>
2026-01-12 15:04:56 +0700

    Update

--- analytics/analytics.py
+++ analytics/analytics.py
@@ -1,11 +1,7 @@
 import time
-
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, count, when
 
-# ===============================
-# 1️⃣ Tạo SparkSession
-# ===============================
 spark = SparkSession.builder \
     .appName("Process Flights Batch") \
     .config("spark.hadoop.dfs.client.use.datanode.hostname", "true") \
@@ -15,24 +11,16 @@ spark = SparkSession.builder \
     .config("spark.hadoop.fs.webhdfs.impl", "org.apache.hadoop.hdfs.web.WebHdfsFileSystem")\
     .getOrCreate()
 
-# ===============================
-# 2️⃣ Đọc tất cả file Parquet trên HDFS
-# ===============================
 start_time = time.time()
 hdfs_path = "webhdfs://localhost:9870/user/data/opdi_clean.parquet"
 flights_df = spark.read.parquet(hdfs_path)
 
 print("Read .parquet successfully")
-# ===============================
-# 3️⃣ Kiểm tra schema
-# ===============================
+
 flights_df.printSchema()
 # Cột có thể: "icao24","flight_id","first_seen","last_seen","dof",
 # "adep_p","ades_p","registration","model","typecode"
 
-# ===============================
-# 4️⃣ Thống kê cơ bản
-# ===============================
 total_rows = flights_df.count()
 print(f"Total flights: {total_rows}")
 
@@ -44,27 +32,16 @@ dep_counts.show(10, truncate=False)
 typecode_counts = flights_df.groupBy("typecode").count().orderBy(col("count").desc())
 typecode_counts.show(10, truncate=False)
 
-# ===============================
-# 5️⃣ Xử lý null/UNKNOWN (nếu còn)
-# ===============================
 flights_df = flights_df.fillna("UNKNOWN", subset=["flight_id","adep_p","ades_p","registration","model","typecode"])
 
-# ===============================
-# 6️⃣ Ví dụ filter dữ liệu
-# ===============================
-# Lấy các chuyến bay từ sân bay "JFK" (hoặc các giá trị cụ thể)
+# Lấy các chuyến bay từ sân bay
 jfk_flights = flights_df.filter(col("adep_p") == "EDDF")
-print("Flights from JFK:", jfk_flights.count())
+print("Flights from EDDF:", jfk_flights.count())
 
-# ===============================
-# 7️⃣ Lưu kết quả gộp lại
-# ===============================
 # output_hdfs = "webhdfs://localhost:9870/user/data/merged_flights.parquet"
 # flights_df.write.mode("overwrite").parquet(output_hdfs)
 
 # print(f"✅ Final merged DataFrame saved to {output_hdfs}")
 print("Run time: ", time.time() - start_time)
-# ===============================
-# 8️⃣ Dừng SparkSession
-# ===============================
+
 spark.stop()
